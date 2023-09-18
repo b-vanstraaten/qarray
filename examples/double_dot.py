87bd5fd37f4640664211ed87df63a45a39fd1cb8
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-09-18 20:04:26 +0100

    getting example to work

--- examples/double_dot.py
+++ examples/double_dot.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,8 +9,8 @@ cdd_non_maxwell = [
     [0.1, 0]
 ]
 cgd_non_maxwell = [
-    [1, 0.3],
-    [0.3, 1]
+    [1, 0.2],
+    [0.2, 1]
 ]
 
 model = DotArray(
@@ -18,16 +19,27 @@ model = DotArray(
 )
 voltage_composer = GateVoltageComposer(n_gate = 2)
 
-vg = voltage_composer.do2d(0, -5, 1, 1000, 1, -5, 1, 1000)
-n = model.ground_state_open(vg)
-z = dot_occupation_changes(n)
+vx_min, vx_max = -3, 1
+vy_min, vy_max = -3, 1
 
-plt.figure()
-plt.imshow(1 - z, origin='lower', cmap='gray', alpha=0.5)
+fig, ax = plt.subplots(1, 4, sharex=True, sharey=True)
+fig.set_size_inches(6, 4)
 
+funcs = [
+    lambda vg: model.ground_state_open(vg),
+    lambda vg: model.ground_state_closed(vg, 1),
+    lambda vg: model.ground_state_closed(vg, 2),
+    lambda vg: model.ground_state_closed(vg, 3),
+]
 
-n = model.ground_state_closed(vg, 10)
-z = dot_occupation_changes(n)
+ax[0].set_ylabel(f'$V_y$')
+for i, func in enumerate(funcs):
+    vg = voltage_composer.do2d(0, vy_min, vx_max, 1000, 1, vy_min, vy_max, 1000)
+    n = func(vg)
+    z = dot_occupation_changes(n)
 
-plt.imshow(z, origin='lower', cmap='Reds', alpha=0.5)
-plt.show()
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "black"])
+    ax[i].imshow(z, extent=[vx_min, vx_max, vy_min, vy_max], origin='lower', aspect='auto', cmap=cmap,
+                 interpolation='antialiased')
+    ax[i].set_aspect('equal')
+    ax[i].set_xlabel(f'$V_x$')
