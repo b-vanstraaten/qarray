d9b18129cc09230f06f136a7fd7f7baa8206393f
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-09-19 18:38:30 +0100

    updating tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,21 +1,20 @@
 import unittest
-
-import numpy as np
 from pathlib import Path
-
 
 class ExampleTests(unittest.TestCase):
 
-    def test_examples(self):
+    def test_all_examples_for_error(self):
         """
         Test to check all the examples run without errors
         :return:
         """
 
         for python_file in Path(__file__).parent.parent.glob('examples/*.py'):
-            with open(python_file, mode = 'r') as f:
-                print(f"Running {python_file.name}")
+            with open(python_file, mode = 'r+') as f:
                 try:
                     exec(f.read())
                 except Exception as e:
                     raise RuntimeError(f"Error in {python_file.name}") from e
+
+if __name__ == '__main__':
+    unittest.main()
