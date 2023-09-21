62809c3785c87271a932503356affa1e14d1340c
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-09-21 18:00:48 +0100

    adding type enforcement

--- src/core_rust/core_rust.py
+++ src/core_rust/core_rust.py
@@ -1,6 +1,7 @@
 """
 Type hinted wrappers for the rust core functions.
 """
+import numpy as np
 from pydantic import NonNegativeInt
 from rusty_capacitance_model_core import (ground_state_open, ground_state_closed,
                                           closed_charge_configurations_brute_force)
@@ -18,8 +19,11 @@ def closed_charge_configurations_brute_force_rust(n_charge: NonNegativeInt, n_do
     :param n_dot: the number of dots in the dot array
     :return: the list of charge configurations
     """
-    # floor_list = floor_list.astype(np.uint64)
+    n_charge = np.int64(n_charge)
+    n_dot = np.int64(n_dot)
+    floor_list = floor_list.astype(np.uint64)
     return VectorList(closed_charge_configurations_brute_force(n_charge, n_dot, floor_list))
+
 
 def ground_state_open_rust(vg: VectorList, cgd: Cgd, cdd_inv: CddInv, threshold: float) -> VectorList:
     """
@@ -30,6 +34,12 @@ def ground_state_open_rust(vg: VectorList, cgd: Cgd, cdd_inv: CddInv, threshold:
     :param threshold: the threshold to use for the ground state calculation
     :return: the lowest energy charge configuration for each gate voltage coordinate vector
     """
+
+    # enforcing the correct type of float64 type, so the rust code to come won't panic
+    vg = vg.astype(np.float64)
+    cgd = cgd.astype(np.float64)
+    cdd_inv = cdd_inv.astype(np.float64)
+    threshold = np.float64(threshold)
     return VectorList(ground_state_open(vg, cgd, cdd_inv, threshold))
 
 
@@ -44,5 +54,9 @@ def ground_state_closed_rust(vg: VectorList, n_charge: NonNegativeInt, cgd: Cgd,
     :param threshold: the threshold to use for the ground state calculation
     :return: the lowest energy charge configuration for each gate voltage coordinate vector
     """
-
+    # enforcing the correct type of float64 type, so the rust code to come won't panic
+    vg = vg.astype(np.float64)
+    cgd = cgd.astype(np.float64)
+    cdd_inv = cdd_inv.astype(np.float64)
+    n_charge = np.int64(n_charge)
     return VectorList(ground_state_closed(vg, n_charge, cgd, cdd_inv))
