2685972edb1a3f1cbe685de117e4c5bb71a8ca0f
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-10-31 12:46:24 +0000

    trying to fix memory issue

--- examples/benchmarks.py
+++ examples/benchmarks.py
@@ -8,10 +8,6 @@ from tqdm import tqdm
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '7'
 
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "true"
-os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "1.0"
-os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
-
 devices = jax.local_devices()
 
 np.random.seed(1)
