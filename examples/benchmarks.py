f2038d37063a0254029c026bcb398d6f907e6372
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-10-31 12:26:22 +0000

    changing example

--- examples/benchmarks.py
+++ examples/benchmarks.py
@@ -81,8 +81,8 @@ def benchmark(core, state, n_dots, n_voltage_points, n_model_max, t_max, plot=Tr
 benchmark_combinations = [
     # ('rust', 'open', np.arange(16, 1, -1)),
     # ('rust', 'closed', np.arange(16, 1, -1)),
-    ('jax', 'open', np.arange(16, 1, -1)),
-    ('jax', 'closed', np.arange(16, 1, -1)),
+    ('jax', 'open', np.arange(12, 1, -1)),
+    ('jax', 'closed', np.arange(12, 1, -1)),
     # ('python', 'open', np.arange(8, 1, -1)),
     # ('python', 'closed', np.arange(8, 1, -1)),
     # ('jax_brute_force', 'open', np.arange(6, 1, -1)),
