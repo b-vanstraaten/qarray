939caeb98b80ba612429d24465d983a610305cc1
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-11-27 15:57:25 +0000

    adding plot

--- examples/free_energy.py
+++ examples/free_energy.py
@@ -1,7 +1,13 @@
 # setting up the constant capacitance model_threshold_1
+import matplotlib
 import matplotlib.patches as patches
-import matplotlib.pyplot as plt
 import numpy as np
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+matplotlib.rc('font', size=7)
+plt.style.use(['science', 'no-latex', 'grid'])
 
 cdd = np.array([
     1, -0.1,
@@ -22,10 +28,9 @@ def Free(N):
     return (np.swapaxes(delta, -1, -2) @ np.linalg.inv(cdd) @ delta).squeeze()
 
 
-x = np.linspace(-0.1, 3.1, 100)
-y = np.linspace(-0.1, 3.1, 100)
+x = np.linspace(0.9, 2.1, 100)
 N = np.stack(np.meshgrid(
-    x, y
+    x, x
 ), axis=-1)
 
 F = Free(N)
@@ -38,13 +43,12 @@ n_cont = cgd @ vg
 
 N = np.stack([n_x, n_y], axis=-1)
 
-fig, ax = plt.subplots(figsize=(3.5, 3.5))
-
-ax.contour(x, y, F, levels=20, linewidths=1, alpha=1., cmap='YlGnBu')
+fig, ax = plt.subplots()
+fig.set_size_inches(3.5 / 2, 3.5 / 2)
+ax.contour(x, x, F, levels=20, linewidths=1, alpha=1., cmap='YlGnBu')
 ax.scatter(n_x.reshape(-1), n_y.reshape(-1), c='k', marker='.')
 ax.scatter(n_cont[0], n_cont[1])
-plt.xlim(x.min(), x.max())
-plt.ylim(y.min(), y.max())
+
 plt.xticks(np.arange(0, 4))
 plt.yticks(np.arange(0, 4))
 
@@ -60,15 +64,17 @@ ax.add_patch(rect_x)
 ax.add_patch(rect_y)
 ax.add_patch(rect_full)
 
-for i in range(4):
-    for j in range(4):
-
-        match (i, j):
-            case (1, 1):
-                ax.annotate(f'({i}, {j})', (i + 0.025, j - 0.1), fontsize=7)
-            case (1, 2):
-                ax.annotate(f'({i}, {j})', (i + 0.025, j + 0.025), fontsize=7)
-            case _:
-                ax.annotate(f'({i}, {j})', (i + 0.025, j), fontsize=7)
-
+# for i in range(4):
+#     for j in range(4):
+#
+#         match (i, j):
+#             case (1, 1):
+#                 ax.annotate(f'({i}, {j})', (i + 0.025, j - 0.1), fontsize=7)
+#             case (1, 2):
+#                 ax.annotate(f'({i}, {j})', (i + 0.025, j + 0.025), fontsize=7)
+#             case _:
+#                 ax.annotate(f'({i}, {j})', (i + 0.025, j), fontsize=7)
+ax.set_xlim(x.min(), x.max())
+ax.set_ylim(x.min(), x.max())
+plt.savefig('free_energy.pdf', bbox_inches='tight')
 plt.show()
