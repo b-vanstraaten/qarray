fixed minus sign issue.

examples/bilayer_example.py
@@ -44,7 +44,7 @@ vg = voltage_composer.do2d(0, vy_min, vx_max, 400, 1, vy_min, vy_max, 400)
 vg_correction = optimal_Vg(model.cdd_inv, model.cgd, np.random.randn(4))
 vg += vg_correction
 
-s = model.ground_state_open(-vg)
+s = model.ground_state_open(vg)
 z = dot_occupation_changes(s)
 
 plt.imshow(z, extent=[vx_min, vx_max, vy_min, vy_max], origin='lower', aspect='auto', cmap='Greys')

examples/charge_sensing.py
@@ -35,7 +35,7 @@ ground_state_funcs = [
 vx_min, vx_max = -5, 5
 vy_min, vy_max = -5, 5
 # using the dot voltage composer to create the dot voltage array for the 2d sweep
-vg = voltage_composer.do2d(0, vy_min, vx_max, 1000, 1, vy_min, vy_max, 1000)
+vg = voltage_composer.do2d(0, vy_min, vx_max, 100, 1, vy_min, vy_max, 100)
 vg += model.optimal_Vg(np.zeros(model.n_dot))
 
 # creating the figure and axes

qarray/brute_force_jax/closed.py
@@ -46,7 +46,7 @@ def _ground_state_closed_0d(vg: jnp.ndarray, cgd: jnp.ndarray, cdd_inv: jnp.ndar
     :param vg: the dot voltage coordinate vector
     :param cgd: the dot to dot capacitance matrix
     :param cdd_inv: the inverse of the dot to dot capacitance matrix
-    :param Cdd: the dot to dot capacitance matrix
+    :param cdd: the dot to dot capacitance matrix
     :param n_charge: the total number of charge carriers in the array
     :return: the lowest energy charge configuration
     """

qarray/brute_force_python/closed.py
@@ -36,7 +36,7 @@ def _ground_state_closed_0d(vg: np.ndarray, cgd: np.ndarray, cdd_inv: np.ndarray
     :param vg: the dot voltage coordinate vector
     :param cgd: the dot to dot capacitance matrix
     :param cdd_inv: the inverse of the dot to dot capacitance matrix
-    :param Cdd: the dot to dot capacitance matrix
+    :param cdd: the dot to dot capacitance matrix
     :param n_charge: the total number of charge carriers in the array
     :return: the lowest energy charge configuration
     """

qarray/classes/ChargeSensedDotArray.py
@@ -30,7 +30,7 @@ class ChargeSensedDotArray(BaseDataClass):
         self.n_sensor = self.Cds.shape[0]
         self.n_gate = self.Cgd.shape[1]
 
-        # checking the shape of the Cgd matrix
+        # checking the shape of the cgd matrix
         assert self.Cgd.shape[
                    0] == self.n_dot, f'Cgd must be of shape (n_dot, n_gate) = ({self.n_dot}, {self.n_gate})'
         assert self.Cgd.shape[
@@ -49,12 +49,12 @@ class ChargeSensedDotArray(BaseDataClass):
                                                                                          self.Cds,
                                                                                          self.Cgs)
 
-        self.Cdd = self.cdd_full[:self.n_dot, :self.n_dot]
+        self.cdd = self.cdd_full[:self.n_dot, :self.n_dot]
         self.cdd_inv = self.cdd_inv_full[:self.n_dot, :self.n_dot]
-        self.Cgd = self.cgd_full[:self.n_dot, :]
+        self.cgd = self.cgd_full[:self.n_dot, :]
 
         if self.threshold == 'auto' or self.threshold is None:
-            self.threshold = compute_threshold(self.Cdd)
+            self.threshold = compute_threshold(self.cdd)
 
     def optimal_Vg(self, n_charges: VectorList, rcond: float = 1e-3) -> np.ndarray:
         """
@@ -63,7 +63,7 @@ class ChargeSensedDotArray(BaseDataClass):
         :param rcond: the rcond parameter for the least squares solver
         :return: the optimal dot voltages of shape (n_gate,)
         """
-        return optimal_Vg(cdd_inv=self.cdd_inv, cgd=self.Cgd, n_charges=n_charges, rcond=rcond)
+        return optimal_Vg(cdd_inv=self.cdd_inv, cgd=self.cgd, n_charges=n_charges, rcond=rcond)
 
     def ground_state_open(self, vg: VectorList | np.ndarray) -> np.ndarray:
         """

qarray/classes/DotArray.py
@@ -6,7 +6,7 @@ from pydantic import NonNegativeInt
 from .BaseDataClass import BaseDataClass
 from ._helper_functions import (_ground_state_open, _ground_state_closed)
 from ..functions import convert_to_maxwell, compute_threshold, optimal_Vg
-from ..qarray_types import Cdd as TypeCdd  # to avoid name clash with dataclass Cdd
+from ..qarray_types import Cdd as TypeCdd  # to avoid name clash with dataclass cdd
 from ..qarray_types import CgdNonMaxwell, CddNonMaxwell, VectorList, Cgd_holes, Cgd_electrons, PositiveValuedMatrix
 
 if TYPE_CHECKING:
@@ -63,45 +63,45 @@ class DotArray(BaseDataClass):
     def __post_init__(self):
         """
         This function is called after the initialization of the dataclass. It checks that the capacitance matrices
-        are valid and sets the cdd_inv attribute as the inverse of Cdd.
+        are valid and sets the cdd_inv attribute as the inverse of cdd.
         """
 
-        # checking that either Cdd and Cgd or Cdd and Cgd are specified
+        # checking that either cdd and cgd or cdd and cgd are specified
         non_maxwell_pair_passed = not both_none(self.Cdd, self.Cgd)
         maxwell_pair_passed = not both_none(self.cdd, self.cgd)
-        assertion_message = 'Either Cdd and Cgd or Cdd and Cgd must be specified'
+        assertion_message = 'Either cdd and cgd or cdd and cgd must be specified'
         assert (non_maxwell_pair_passed or maxwell_pair_passed), assertion_message
 
         # if the non maxwell pair is passed, convert it to maxwell
         if non_maxwell_pair_passed:
             self.cdd, self.cdd_inv, self.cgd = convert_to_maxwell(self.Cdd, self.Cgd)
 
-        # setting the cdd_inv attribute as the inverse of Cdd
+        # setting the cdd_inv attribute as the inverse of cdd
         self.cdd_inv = np.linalg.inv(self.cdd)
 
-        # checking that the Cgd matrix has all positive or all negative elements
+        # checking that the cgd matrix has all positive or all negative elements
         # so that the sign can be matched to the charge carrier
-        assert all_positive_or_negative(self.cgd), 'The elements of Cgd must all be positive or all be negative'
+        assert all_positive_or_negative(self.cgd), 'The elements of cgd must all be positive or all be negative'
 
-        # matching the sign of the Cgd matrix to the charge carrier
+        # matching the sign of the cgd matrix to the charge carrier
         match self.charge_carrier.lower():
             case 'electrons' | 'electron' | 'e' | '-':
                 self.charge_carrier = 'electrons'
-                # the Cgd matrix is positive for electrons
+                # the cgd matrix is positive for electrons
                 self.cgd = Cgd_electrons(np.abs(self.cgd))
             case 'holes' | 'hole' | 'h' | '+':
                 self.charge_carrier = 'holes'
-                # the Cgd matrix is negative for holes
+                # the cgd matrix is negative for holes
                 self.cgd = Cgd_holes(-np.abs(self.cgd))
             case _:
                 raise ValueError(f'charge_carrier must be either "electrons" or "holes {self.charge_carrier}"')
 
-        # by now in the code, the Cdd and Cgd matrices have been initialized as their specified types. These
-        # types enforce most of the constraints on the matrices like positive-definitness for Cdd for example,
+        # by now in the code, the cdd and cgd matrices have been initialized as their specified types. These
+        # types enforce most of the constraints on the matrices like positive-definitness for cdd for example,
         # but not all. The following asserts check the remainder.
         self.n_dot = self.cdd.shape[0]
         self.n_gate = self.cgd.shape[1]
-        assert self.cgd.shape[0] == self.n_dot, 'The number of dots must be the same as the number of rows in Cgd'
+        assert self.cgd.shape[0] == self.n_dot, 'The number of dots must be the same as the number of rows in cgd'
 
         # checking that the threshold is valid
         match self.threshold:

qarray/classes/_helper_functions.py
@@ -126,8 +126,8 @@ def _ground_state_closed(model, vg: VectorList | np.ndarray, n_charge: NonNegati
     match model.core:
         case 'rust' | 'Rust' | 'RUST' | 'r':
             result = ground_state_closed_rust(
-                vg=vg, n_charge=n_charge, cgd=model.Cgd,
-                cdd=model.Cdd, cdd_inv=model.cdd_inv,
+                vg=vg, n_charge=n_charge, cgd=model.cgd,
+                cdd=model.cdd, cdd_inv=model.cdd_inv,
                 threshold=model.threshold, polish=model.polish, T=model.T
             )
 
@@ -135,8 +135,8 @@ def _ground_state_closed(model, vg: VectorList | np.ndarray, n_charge: NonNegati
             if model.threshold < 1.:
                 print('Warning: JAX core does not support threshold < 1.0, using of 1.0')
             result = ground_state_closed_jax(
-                vg=vg, n_charge=n_charge, cgd=model.Cgd,
-                cdd=model.Cdd, cdd_inv=model.cdd_inv, T=model.T, batch_size=model.batch_size
+                vg=vg, n_charge=n_charge, cgd=model.cgd,
+                cdd=model.cdd, cdd_inv=model.cdd_inv, T=model.T, batch_size=model.batch_size
             )
 
         case 'brute_force_jax' | 'jax_brute_force' | 'Jax_brute_force' | 'JAX_BRUTE_FORCE' | 'b':
@@ -144,8 +144,8 @@ def _ground_state_closed(model, vg: VectorList | np.ndarray, n_charge: NonNegati
                 print('Warning: JAX core does not support threshold < 1.0, using threshold of 1.0')
 
             result = ground_state_closed_brute_force_jax(
-                vg=vg, n_charge=n_charge, cgd=model.Cgd,
-                cdd=model.Cdd, cdd_inv=model.cdd_inv, T=model.T, batch_size=model.batch_size
+                vg=vg, n_charge=n_charge, cgd=model.cgd,
+                cdd=model.cdd, cdd_inv=model.cdd_inv, T=model.T, batch_size=model.batch_size
             )
 
         case 'python_brute_force' | 'Python_brute_force' | 'PYTHON_BRUTE_FORCE' | 'bp' | 'brute_force_python' | 'Brute_force_python' | 'BRUTE_FORCE_PYTHON' | 'bpy':
@@ -158,14 +158,14 @@ def _ground_state_closed(model, vg: VectorList | np.ndarray, n_charge: NonNegati
                 print('Warning: JAX core does not support threshold < 1.0, using threshold of 1.0')
 
             result = ground_state_closed_brute_force_python(
-                vg=vg, n_charge=n_charge, cgd=model.Cgd,
-                cdd=model.Cdd, cdd_inv=model.cdd_inv, T=model.T, batch_size=model.batch_size
+                vg=vg, n_charge=n_charge, cgd=model.cgd,
+                cdd=model.cdd, cdd_inv=model.cdd_inv, T=model.T, batch_size=model.batch_size
             )
 
         case 'python' | 'Python' | 'PYTHON' | 'p':
             result = ground_state_closed_python(
-                vg=vg, n_charge=n_charge, cgd=model.Cgd,
-                cdd=model.Cdd, cdd_inv=model.cdd_inv,
+                vg=vg, n_charge=n_charge, cgd=model.cgd,
+                cdd=model.cdd, cdd_inv=model.cdd_inv,
                 threshold=model.threshold, polish=model.polish
             )
         case _:

qarray/functions.py
@@ -100,7 +100,7 @@ def convert_to_maxwell_with_sensor(cdd_non_maxwell: CddNonMaxwell, cgd_non_maxwe
     cdd_non_maxwell_full[:n_dot, n_dot:] = cds_non_maxwell.T
     cdd_non_maxwell_full = CddNonMaxwell(cdd_non_maxwell_full)
 
-    # populating the Cgd matrix, with zeros for the sensor dots
+    # populating the cgd matrix, with zeros for the sensor dots
     cgd_non_maxwell_full = np.zeros((n_dot + n_sensor, n_gate))
     cgd_non_maxwell_full[:n_dot, :] = cgd_non_maxwell
     cgd_non_maxwell_full[n_dot:, :] = cgs_non_maxwell
@@ -109,7 +109,8 @@ def convert_to_maxwell_with_sensor(cdd_non_maxwell: CddNonMaxwell, cgd_non_maxwe
     return convert_to_maxwell(cdd_non_maxwell_full, cgd_non_maxwell_full)
 
 
-def convert_to_maxwell(cdd_non_maxwell: CddNonMaxwell, cgd_non_maxwell: CgdNonMaxwell) -> (Cdd, Cgd_holes):
+def convert_to_maxwell(cdd_non_maxwell: CddNonMaxwell, cgd_non_maxwell: CgdNonMaxwell) -> (
+        Cdd, CddInv, NegativeValuedMatrix):
     """
     Function to convert the non Maxwell capacitance matrices to their maxwell form.
     :param cdd_non_maxwell: