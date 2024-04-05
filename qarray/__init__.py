9987ad8cb9f4819eb1f4879eef08241fc712b08c
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2024-04-05 09:57:25 +0100

    updating readmes

--- qarray/__init__.py
+++ qarray/__init__.py
@@ -2,7 +2,7 @@
 Qarray, a GPU accelerated quantum dot array simulator, leveraging parallelised Rust and JAX XLA acceleration
 to compute charge stability diagrams of large both open and closed arrays in milliseconds.
 """
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 from .classes import (DotArray, GateVoltageComposer, ChargeSensedDotArray)
 from .functions import (optimal_Vg, compute_threshold, convert_to_maxwell, dot_occupation_changes, lorentzian,
