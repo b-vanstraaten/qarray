1e5fe3e7dbece5e1f1e0d20b280b165f27f6fd29
Cornelius Carlsson <spet6029@ox.ac.uk>
2024-06-01 00:53:27 +0100

    Revised telegrapher noise implementation

--- qarray/noise_models/TelegraphNoise.py
+++ qarray/noise_models/TelegraphNoise.py
@@ -29,20 +29,24 @@ class TelegraphNoise(BaseNoiseModel):
     def sample_input_noise(self, shape):
         """
         Sample input noise from the telegraph noise model. This noise perturbs the potential of the dots in the charge sensor,
-        before those potentials are used in the Lorenzian function to calculate the charge sensor output.
+        before those potentials are used in the Lorentzian function to calculate the charge sensor output.
         """
 
         n_sensors = shape[-1]
         n_data_points = np.prod(shape[:-1])
 
-        noise = np.zeros(shape=(n_data_points, n_sensors))
-        for n in range(n_sensors):
-            for i in range(1, n_data_points):
-                if noise[i - 1, n] == 0:
-                    p = self.p01
-                else:
-                    p = self.p10
+        noise = np.zeros((n_data_points, n_sensors))
 
-                noise[i, n] = (np.random.choice([0, 1], p=[1 - p, p]) + noise[i - 1, n]) % 2
+        for charge_sensor in range(n_sensors):
+            i = 0
+            state = 0
+
+            while i < n_data_points:
+                p = self.p01 if state == 0 else self.p10
+                n_same_state = np.random.geometric(p)
+                end = min(i + n_same_state, n_data_points)
+                noise[i:end, charge_sensor] = state
+                state = 1 - state  # Switch state
+                i = end
 
         return self.amplitude * noise.reshape(*shape)
