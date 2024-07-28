86064ddfa34f9f2491df35af66a17489c5aab509
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2024-07-28 21:43:52 +0200

    updating example

--- examples/charge_sensing.py
+++ examples/charge_sensing.py
@@ -5,9 +5,9 @@ from qarray import ChargeSensedDotArray
 
 # defining the capacitance matrices
 Cdd = [[0., 0.1], [0.1, 0.]]  # an (n_dot, n_dot) array of the capacitive coupling between dots
-Cgd = [[1., 0.2, 0.05], [0.2, 1., 0.05], ]  # an (n_dot, n_gate) array of the capacitive coupling between gates and dots
+Cgd = [[1., 0.6, 0.05], [0.2, 1., 0.05], ]  # an (n_dot, n_gate) array of the capacitive coupling between gates and dots
 Cds = [[0.02, 0.01]]  # an (n_sensor, n_dot) array of the capacitive coupling between dots and sensors
-Cgs = [[0.06, 0.05, 1]]  # an (n_sensor, n_gate) array of the capacitive coupling between gates and sensor dots
+Cgs = [[0.06, 0.0, 1]]  # an (n_sensor, n_gate) array of the capacitive coupling between gates and sensor dots
 
 # creating the model
 model = ChargeSensedDotArray(
@@ -15,11 +15,11 @@ model = ChargeSensedDotArray(
     coulomb_peak_width=0.05, T=100
 )
 
-model.gate_voltage_composer.virtual_gate_matrix = np.array([
-    [1, -0.0, 0],
-    [-0.0, 1, 0],
-    [-0.06, -0.05, 0],
-])
+# model.gate_voltage_composer.virtual_gate_matrix = np.array([
+#     [1, -0.0, 0],
+#     [-0.0, 1, 0],
+#     [-0.06, -0.05, 0],
+# ])
 
 # defining the min and max values for the dot voltage sweep
 vx_min, vx_max = -2, 2
