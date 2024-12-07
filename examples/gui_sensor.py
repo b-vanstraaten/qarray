05860c21add1a641ddf9edac150a1593c1b699f3
b-lvanstraaten <b.l.vanstraaten@tudelft.nl>
2024-12-07 12:07:39 +0100

    minor bug fix to do with the switching between the automatically calculated virtual gate matrix and not in the charge sensor gui.
    pypi

--- examples/gui_sensor.py
+++ examples/gui_sensor.py
@@ -12,7 +12,7 @@ Cgd = [[1., 0.1, 0.05], [0.1, 1., 0.05]]  # an (n_dot, n_gate) array of the capa
 Cds = [[0.05, 0.00]]  # an (n_sensor, n_dot) array of the capacitive coupling between dots and sensors
 Cgs = [[0.06, 0.05, 1]]  # an (n_sensor, n_gate) array of the capacitive coupling between gates and sensor dots
 
-noise_model = WhiteNoise(amplitude=1e-2) + TelegraphNoise(p01=5e-5, p10=5e-3, amplitude=1e-2)
+noise_model = WhiteNoise(amplitude=1e-2)
 
 # creating the model
 model = ChargeSensedDotArray(
